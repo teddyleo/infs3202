dc2e3024527e98d25d9ca05afbf981e2c0ccbf6c
Jess Ahrens <jtahrens@hotmail.com>
2016-06-01 13:46:07 +1000

    Added 2 riddles

---
+++ youwin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Occhiolist - Success</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Droid+Serif" />
+		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto" />
+		<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+		<link href="css/riddlecontent.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div class="content">
+			<div class="riddle">
+				<h1>Success</h1>
+				<p>You made it through the riddles, well done.
+				</p>
+				<a id="help" href="home.php">Go Back to Homepage</a>
+			</div>
+			<div class="push"></div>
+		</div>
+        <div class="footer">
+            <div class="copyright">
+				
+			</div>
+        </div>
+	</body>
+</html>
